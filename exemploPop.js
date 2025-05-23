d32e8b330605ceca927c8a070c30df41f0671fd2
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-23 08:36:54 -0300

    Adiciona o arquivo de metodosArray

---
+++ exemploPop.js
@@ -0,0 +1,9 @@
+let apps = ["Instagram", " WhatsApp", " Tik Tok"];
+
+console.log(`Antes do pop: ${apps}`);
+
+let appRemovido = apps.pop();
+
+console.log(`Depois do pop: ${apps}`);
+console.log(`Música Removida: ${appRemovido}`);
+
---
+++ exemploPush.js
@@ -0,0 +1,7 @@
+let playlist = ["Funk", " Sertanejo", " Trap"];
+
+console.log(`Antes do push: ${playlist}`);
+
+playlist.push(" Rock");
+
+console.log(`Depois do push: ${playlist}`);
