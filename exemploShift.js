8d51be29e73fcbc0353cb597778cf12b2736aab3
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-23 08:57:28 -0300

    Adiciona o arquivo de metodosArray

---
+++ exemploShift.js
@@ -0,0 +1,9 @@
+let jogos = ["Brawl Stars", " Fortnite", " Mortal Kombat" ];
+
+console.log(`Antes do shift: ${jogos}`);
+
+let jogoRemovido = jogos.shift();
+
+console.log(`Depois do shift: ${jogos}`);
+console.log(`Jogo removido: ${jogoRemovido}`);
+
---
+++ exemploUnshift.js
@@ -0,0 +1,7 @@
+let series = ["La casa de papel", " Flash"];
+
+console.log(`Antes do unshift: ${series}`);
+
+series.unshift("Arrow ");
+
+console.log(`Depois do unshift: ${series}`);
